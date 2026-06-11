4a6f9c75b2f98cc7d40a57217145697c59074a76
Sanaullah <yourgithubemail@gmail.com>
2026-06-11 20:51:21 +0500

    new update

--- src/components/Home/TrustedSection.jsx
+++ src/components/Home/TrustedSection.jsx
@@ -15,7 +15,7 @@ export default function TrustedSection() {
       <div className="py-16 sm:px-40">
         <h1 className="text-center text-gray">Since 2017, Trusted by:</h1>
 
-        <div className=" mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-8 gap-y-10  ">
+        <div className=" mx-auto grid sm:grid-cols-2 md2:grid-cols-3 lg:grid-cols-5 mt-8 gap-y-10  ">
           {logos.map((logo, index) => (
             <div key={index} className="flex items-center justify-center">
               <Image
@@ -30,36 +30,21 @@ export default function TrustedSection() {
           ))}
         </div>
       </div>
-      <div
-  className="bg-darkblue text-white flex flex-wrap items-center justify-center
-             gap-8 md:gap-16 lg:gap-64
-             max-w-7xl mx-auto px-6 py-8"
->
-  <div className="space-y-2 max-w-md">
-    <p>
-      (...) Working with them has been a pleasure. The working
-      relationship and level of trust we've built up are outstanding.
-      TeaCode has been super responsive to what we want to do and has a
-      vested interest in our goals.
-    </p>
-
-    <h1 className="font-extrabold mt-10">
-      Richard Ganpatsing
-    </h1>
-
-    <span>CTO</span>
-  </div>
-
-  <div className="shrink-0">
-    <Image
-      src="/Home/man.png"
-      alt="Richard Ganpatsing"
-      width={300}
-      height={700}
-      className=" relative bottom-32 left-10 md:bottom-0 md:left-0 w-64 lg:w-72 h-auto"
-    />
-  </div>
-</div>
+      <div className="bg-darkblue px-1 pt-5   text-white flex flex-wrap  justify-center items-center sm:gap-10  md:gap-44 lg:gap-64 max-w-7xl mx-auto   ">
+        <div className="space-y-2">
+        <p className="w-70">
+          (...) Working with them has been a pleasure. The working relationship
+          and level of trust we've built up are outstanding. TeaCode has been
+          super responsive to what we want to do and has a vested interest in
+          our goals.
+        </p>
+        <h1 className="font-extrabold mt-10">Richard Ganpatsing</h1>
+        <span>CTO</span>
+        </div>
+        <div >
+          <Image src={"/Home/man.png"} width={300} height={700} alt=""/>
+        </div>
+      </div>
     </section>
   );
 }
